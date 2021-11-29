83fd99d99c7d02152422ddb2a6962d70bac6b9df
Fran Arbanas <fran.arbanas@axilis.com>
2021-11-29 21:46:15 +0100

    feat: add upload file

--- preprocess/urls.py
+++ preprocess/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'preprocess'
 urlpatterns = [
     path('', views.list_files, name='list'),
+    path('upload', views.upload_file, name='upload_file'),
     path('results', views.preprocess_results, name='preprocess_results')
 ]
--- preprocess/views.py
+++ preprocess/views.py
@@ -12,12 +12,8 @@ from genGroup.settings import MEDIA_ROOT
 from .forms import SelectFileForm, UploadFileForm
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the preprocess index.")
-
-
-def handle_uploaded_file(f):
-    with open('some/file/name.txt', 'wb+') as destination:
+def handle_uploaded_file(f, file_name):
+    with open(os.path.join(MEDIA_ROOT, file_name), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
@@ -26,55 +22,36 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponse('/success/url/')
+            handle_uploaded_file(
+                request.FILES['file'], form.cleaned_data.get('title'))
+            request.session['files'] = form.cleaned_data.get('title')
+            return HttpResponseRedirect('results')
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
 
 
-class FileFieldFormView(FormView):
-    form_class = UploadFileForm
-    template_name = 'upload.html'  # Replace with your template.
-    success_url = '/preprocess/list'  # Replace with your URL or reverse().
-
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('file_field')
-        if form.is_valid():
-            for f in files:
-                handle_uploaded_file(f)
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-
 def list_files(request):
-    #fastq_files = os.listdir(settings.MEDIA_ROOT)
-    # context = {
-    #    'files': fastq_files,
-    # }
-
     if request.method == 'POST':
         form = SelectFileForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/choose/')
+            request.session['files'] = request.POST['files']
+            return HttpResponseRedirect('results')
     else:
         form = SelectFileForm()
 
-    return render(request, 'preprocess/default_view.html', {'form': form})
+    return render(request, 'default_view.html', {'form': form})
 
 
 def preprocess_results(request):
-    file = request.POST['files']
+    file = request.session.get('files')
     with open(os.path.join(MEDIA_ROOT, file)) as f:
         result = preprocess(f)
 
     result = dict(
         sorted(result.items(), key=lambda item: item[1], reverse=True))
     context = {'result': result}
-    return render(request, 'preprocess/preprocess_results.html', context)
+    return render(request, 'preprocess_results.html', context)
 
 
 def preprocess(fastqfile):
@@ -87,5 +64,4 @@ def preprocess(fastqfile):
 
         counts[seq_len] += 1
 
-    print(counts)
     return counts
