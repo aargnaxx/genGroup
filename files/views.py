87be2e9639c7eb56fb99e355fa5708b52f573711
Fran Arbanas <fran.arbanas@axilis.com>
2022-01-14 12:29:16 +0100

    chore: cleanup

--- files/views.py
+++ files/views.py
@@ -31,23 +31,3 @@ class FileView(APIView):
         else:
             print('error', file_serializer.errors)
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-def handle_uploaded_file(f, file_name):
-    with open(os.path.join(MEDIA_ROOT, file_name), 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-
-
-def upload(request):
-    if request.method == 'POST':
-        #form = UploadFileForm(request.POST, request.FILES)
-        #if form.is_valid():
-        ##    title = form.cleaned_data.get('title')
-        #    handle_uploaded_file(request.FILES['file'], title)
-        #    request.session['files'] = title
-        #    return HttpResponseRedirect('results')
-        pass
-    else:
-        #form = UploadFileForm()
-        pass
-    return render(request, 'preprocess/upload.html')
