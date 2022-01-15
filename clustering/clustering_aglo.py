abe00a6f7ca5aeff0ca61d647b97b70f99cad470
Sandi Letić <sandi.letic@fer.hr>
2022-01-15 18:36:44 +0100

    add aglo clustering, refactor medoids

--- clustering/clustering_aglo.py
+++ clustering/clustering_aglo.py
@@ -1,28 +1,25 @@
 from preprocess.scoring import Scoring, pcoa_calculate_dissimilarity
 from sklearn.metrics import pairwise_distances
-from sklearn_extra.cluster import KMedoids
+from sklearn.cluster import AgglomerativeClustering
 from collections import Counter
 
 
-class ClusteringKm:
+class ClusteringAglo:
     def __init__(self, num_clusters, scores, reading_length):
         diss = pcoa_calculate_dissimilarity(scores, reading_length)
         pd = pairwise_distances(diss)
         self.num_clusters = num_clusters
-        self.kmedoids = KMedoids(num_clusters, metric='precomputed')
-        # self.results = KMedoids.get_params(self.kmedoids)
-        self.results = self.kmedoids.fit(pd)
+        self.aglo = AgglomerativeClustering(num_clusters, affinity='precomputed', linkage='complete')
+        self.aglo.fit(pd)
 
     def print(self):
-        print(self.results)
-        print(self.results.labels_)
-        # print(self.kmedoids.cluster_centers_)
+        print(self.aglo.labels_)
 
     def save_result(self, output_file):
         lines = str(self.num_clusters) + "\n---\n"
-        lines += ", ".join(str(x) for x in self.results.labels_)
+        lines += ", ".join(str(x) for x in self.aglo.labels_)
         lines += "\n---\n"
-        c = Counter(self.results.labels_)
+        c = Counter(self.aglo.labels_)
         print(c)
         for i in range(self.num_clusters):
             lines += f'{i}({c[i]})\n'
@@ -33,9 +30,9 @@ class ClusteringKm:
 def main(input_file, reading_length):
     sc = Scoring(input_file, reading_length)
     sc.score_calc()
-    cl = ClusteringKm(3, sc.score, reading_length)
+    cl = ClusteringAglo(3, sc.score, reading_length)
     cl.print()
-    cl.save_result('result.txt')
+    cl.save_result('result_aglo.txt')
 
 
 if __name__ == "__main__":
